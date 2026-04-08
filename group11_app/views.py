62d5d314cc83656f687bf71c69e9e7b5af455586
aaronmad914 <s389992@students.cdu.edu.au>
2026-04-08 19:07:13 +0930

    Added stub views and urls.py for basic Django template routing testing.

--- group11_app/views.py
+++ group11_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+"""
+Current views are for testing template page routing.
+"""
+def home(request):
+    return render(request, "home.html")
+
+def recording_list(request):
+    return render(request, "recordings/recording_list.html")
+
+def recording_create(request):
+    return render(request, "recordings/recording_form.html")
+
+def recording_detail(request, pk):
+    return render(request, "recordings/recording_detail.html")
+
+def species_list(request):
+    return render(request, "species/species_list.html")
+
+def species_detail(request, pk):
+    return render(request, "species/species_detail.html")
+
+def anomaly_list(request):
+    return render(request, "anomalies/anomaly_list.html")
+
+def anomaly_create(request, pk):
+    return render(request, "anomalies/anomaly_form.html")

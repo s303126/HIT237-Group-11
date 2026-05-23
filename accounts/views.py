4f7996a1d7ae0dd6f08465fc9d2b97e4261815d8
MelBardoux <s329560@students.cdu.edu.au>
2026-05-23 18:22:42 +1000

    Add authentication navigation with login/logout/signup links

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 from group11_app.models import User
 
@@ -8,4 +9,9 @@ class SignupView(CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = 'accounts/signup.html'
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('home')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
